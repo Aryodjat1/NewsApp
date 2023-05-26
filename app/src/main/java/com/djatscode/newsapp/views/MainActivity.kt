878ded81cd2980d78318b1c20c711add6cd59ace
AryoDjat1 <aryodjati5@gmail.com>
2023-05-26 17:54:49 +0700

    done headlines

--- app/src/main/java/com/djatscode/newsapp/views/MainActivity.kt
+++ app/src/main/java/com/djatscode/newsapp/views/MainActivity.kt
@@ -3,16 +3,23 @@ package com.djatscode.newsapp.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.bumptech.glide.Glide
 import com.djatscode.newsapp.R
+import com.djatscode.newsapp.databinding.ActivityMainBinding
 import com.djatscode.newsapp.models.headline.HeadlineModel
 import com.djatscode.newsapp.networks.ApiClient
 import com.djatscode.newsapp.networks.ApiInterface
+import kotlinx.android.synthetic.main.activity_main.img_headline
+import kotlinx.android.synthetic.main.activity_main.tv_author_headline
+import kotlinx.android.synthetic.main.activity_main.tv_date_headline
+import kotlinx.android.synthetic.main.activity_main.txt_title_headline
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
+    private var binding : ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +35,21 @@ class MainActivity : AppCompatActivity() {
                 ) {
                     val headline = response.body()?.articles
                     Log.d("Hit Headline", "Headline size ${headline?.size}")
+
+                    val imageurl = response.body()?.articles?.get(0)?.urlToImage.toString()
+
                     // bind views
+                    binding.apply {
+                        Glide.with(this@MainActivity)
+                            .load(imageurl)
+                            .centerCrop()
+                            .into(img_headline)
+
+                        txt_title_headline.text = headline?.get(0)?.title
+                        tv_author_headline.text = headline?.get(0)?.author
+                        tv_date_headline.text = headline?.get(0)?.publishedAt
+                    }
+
                 }
 
                 override fun onFailure(call: Call<HeadlineModel>, t: Throwable) {
